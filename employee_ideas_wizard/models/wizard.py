4b917bfe6c51016e9ab99a3b8f9c6357c2511a22
gerryagustian97 <31478259+gerryagustian97@users.noreply.github.com>
2018-04-15 16:49:56 +0700

    Tampilan give vote

--- employee_ideas_wizard/models/wizard.py
+++ employee_ideas_wizard/models/wizard.py
@@ -3,5 +3,12 @@ from odoo import models, fields, api
 class Wizard(models.TransientModel):
 	_name = 'employee_ideas.wizard'
 	
-	rating = fields.Integer('Rating')
-	comments = fields.Char('Comments')
+	rating = fields.Selection([
+			('worst', 'Worst'),
+			('very_bad', 'Very Bad'),
+            ('bad', 'Bad'),
+            ('not_bad', 'Not Bad'),
+            ('good', 'Good'),
+            ('very_good', 'Very Good'),
+            ],default='worst')
+	comments = fields.Char('Comments', required=True)
