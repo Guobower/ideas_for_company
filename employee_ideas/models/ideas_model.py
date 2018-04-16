30467255de83c11de79098324faf540024584a90
MufidJamaluddin <mufid.jamaluddin@gmail.com>
2018-04-16 12:31:34 +0700

    Mendapatkan Departemen, Employee melalui res_user.id dan menambhakan readonly serta akibat dari readonly

--- employee_ideas/models/ideas_model.py
+++ employee_ideas/models/ideas_model.py
@@ -16,17 +16,66 @@ class EmployeeIdeas(models.Model):
             ('closed', 'Closed'),
             ],default='new')
 	title = fields.Char('Title', required=True)
-	employee = fields.Many2one('hr.employee', string='Employee', default=lambda self: self.env.uid)
-	create_date = fields.Date('Create Date', default=fields.Date.today())
-	company = fields.Many2one('res.company', string='Company', default=lambda self: self.env.user.company_id.name)
-	department = fields.Many2one('hr.department', 'Department')
-	deadline = fields.Date('Deadline', required=True, default = datetime.now() + timedelta(days=14))
+	employee = fields.Many2one('hr.employee', string='Employee', default=lambda self: self._get_default_employee(), store=True, readonly=True)
+	create_date = fields.Date('Create Date', default=fields.Date.today(), store=True, readonly=True)
+	company = fields.Many2one('res.company', string='Company', default=lambda self: self.env.user.company_id, store=True, readonly=True)
+	department = fields.Many2one('hr.department', 'Department', default=lambda self: self._get_default_department(), store=True, readonly=True)
+	deadline = fields.Date('Deadline', required=True, default = datetime.now() + timedelta(days=14), store=True, readonly=True)
 	idea_type = fields.Many2one('idea.types', 'Idea Type')
 	details = fields.Char('Details', required=True)
-	votes = fields.Integer('Votes')
-	comments = fields.Char('Comments')
-	rating = fields.Char('Rating')
+	votes = fields.One2many('employee.ideas.votes', string='Votes')
 	tree_notebook = fields.One2many('employee.ideas', 'employee')
+	
+	@api.model
+	def _get_default_employee(self)  :
+		user_id = self.env.uid
+		employee_id = self._cr.execute('SELECT hr_employee.id FROM hr_employee, resource_resource, res_user WHERE hr_employee.id = resource_resource.id AND resource_resource.user_id = ' + user_id)
+		return employee_id
+		
+	@api.model
+	def _get_default_department(self) :
+		user_id = self.env.uid
+		department_id = self._cr.execute('SELECT hr_department.id FROM hr_department, hr_employee WHERE hr_department.id = hr_employee.department_id AND hr_employee.id ' = self.employee)
+		return department_id
+		
+	@api.multi
+	def write(self, vals) :
+		if vals.get('employee', False) :
+			vals['employee'] = self._get_default_employee()
+			
+		if vals.get('create_date', False) :
+			vals['create_date'] = fields.Date.today()
+			
+		if vals.get('company', False) :
+			vals['company'] = self.env.user.company_id
+			
+		if vals.get('department', False) :
+			vals['department'] = self._get_default_department()
+			
+		if vals.get('deadline', False) :
+			vals['deadline'] = datetime.now() + timedelta(days=14)
+		
+		return super(EmployeeIdeas, self).write(vals)
+		
+	@api.model
+	def create(self, vals) :
+		if vals['employee'] is None:
+			vals['employee'] = self._get_default_employee()
+			
+		if vals['create_date'] is None :
+			vals['create_date'] = fields.Date.today()
+			
+		if vals['company'] is None :
+			vals['company'] = self.env.user.company_id
+			
+		if vals['department'] is None :
+			vals['department'] = self._get_default_department()
+			
+		if vals['deadline'] is None :
+			vals['deadline'] = datetime.now() + timedelta(days=14)
+		
+		return super(EmployeeIdeas, self).write(vals)
+		
 	
 	def waiting_progressbar(self):
 		self.write({'state': 'waiting'})
---
+++ employee_ideas_wizard/models/votes.py
@@ -0,0 +1,41 @@
+from odoo import models, fields, api
+
+class Votes(models.Model):
+	_name = 'employee.ideas.votes'
+	
+	ideas_id = fields.Many2one('employee.ideas','Ideas')
+	
+    employee = fields.Many2one('hr.employee', string='Employee', default=lambda self: self._get_default_employee(), readonly=True, store=True)
+	
+    department = fields.Many2one('hr.department', 'Department')
+	
+	rating = fields.Selection([
+			(0, 'Worst'),
+			(1, 'Very Bad'),
+            (2, 'Bad'),
+            (3, 'Not Bad'),
+            (4, 'Good'),
+            (5, 'Very Good'),
+            ],default=0)
+
+	comments = fields.Char('Comments', required=True)
+	
+	@api.model
+	def _get_default_employee(self)  :
+		user_id = self.env.uid
+		employee_id = self._cr.execute('SELECT hr_employee.id FROM hr_employee, resource_resource, res_user WHERE hr_employee.id = resource_resource.id AND resource_resource.user_id = ' + user_id)
+		return employee_id
+		
+	@api.multi
+	def write(self, vals) :
+		if vals.get('employee', False) :
+			vals['employee'] = self._get_default_employee()
+		
+		return super(EmployeeIdeas, self).write(vals)
+		
+	@api.model
+	def create(self, vals) :
+		if vals['employee'] is None:
+			vals['employee'] = self._get_default_employee()
+		
+		return super(EmployeeIdeas, self).write(vals)
