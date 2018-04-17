b3f145f351db74a645e6bfbe5e3448957c8104b4
MufidJamaluddin <mufid.jamaluddin@gmail.com>
2018-04-17 09:22:17 +0700

    Add Default

--- employee_ideas_app/models/ideas_model.py
+++ employee_ideas_app/models/ideas_model.py
@@ -18,10 +18,10 @@ class EmployeeIdeas(models.Model):
             ('closed', 'Closed'),
             ],default='new')
 	title = fields.Char('Title', required=True)
-	employee = fields.Many2one('hr.employee', string='Employee', default=lambda self: self.env.uid)
+	employee = fields.Many2one('hr.employee', string='Employee', default=lambda self: self._get_default_employee())
 	create_date = fields.Date('Create Date', default=fields.Date.today())
 	company = fields.Many2one('res.company', string='Company', default=lambda self: self.env.user.company_id.name)
-	department = fields.Many2one('hr.department', 'Department')
+	department = fields.Many2one('hr.department', 'Department', default=lambda self: self._get_default_department())
 	deadline = fields.Date('Deadline', required=True, default = datetime.now() + timedelta(days=14))
 	idea_type = fields.Many2one('idea.types', 'Idea Type')
 	details = fields.Char('Details', required=True)
@@ -30,6 +30,18 @@ class EmployeeIdeas(models.Model):
 	rating = fields.Char('Rating')
 	tree_notebook = fields.One2many('employee.ideas', 'employee')
 	list_vote = fields.One2many('employee_ideas.wizard', 'id_idea', string='Votes', readonly=True)
+	
+	@api.model
+	def _get_default_employee(self)  :
+		user_id = self.env.uid
+		employee_id = self._cr.execute('SELECT hr_employee.id FROM hr_employee, resource_resource, res_user WHERE hr_employee.id = resource_resource.id AND resource_resource.user_id = ' + user_id)
+		return employee_id
+		
+	@api.model
+	def _get_default_department(self) :
+		user_id = self.env.uid
+		department_id = self._cr.execute('SELECT hr_department.id FROM hr_department, hr_employee WHERE hr_department.id = hr_employee.department_id AND hr_employee.id = ' + self.employee)
+		return department_id
 
 	@api.multi
 	def waiting_progressbar(self):
